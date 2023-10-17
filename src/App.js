672f6a11ec34d2ade3703b779f812bf270310fbe
Radha M <radha.m@tavant.com>
2023-10-17 15:46:59 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import StartButton from './components/StartButton';
-import Form from './components/Form';
 import Form1 from './components/Form1';
+import './css/App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>App Comp</h1>
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<StartButton/>}/>
-        <Route path="form" element={<Form/>}/>
         <Route path='form1' element={<Form1/>}/>
       </Routes>
       </BrowserRouter>
